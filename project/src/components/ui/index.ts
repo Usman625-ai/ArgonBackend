33766db27aec611e16cc46443b4ac953be759d18
usmanhussain <mrusmanhussain101@gmail.com>
2026-07-25 16:23:38 +0500

    remove project folder

--- project/src/components/ui/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as Button } from './Button';
-export { default as Input } from './Input';
-export { default as Badge } from './Badge';
-export { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from './Card';
-export { Modal } from './Modal';
-export { Skeleton, CardSkeleton, TableSkeleton, ProductCardSkeleton } from './Skeleton';
-export { OrderStatusBadge, SellerStatusBadge, PaymentStatusBadge } from './StatusBadge';
--- project/src/pages/admin/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as AdminDashboard } from './Dashboard';
-export { default as SellersPage } from './SellersPage';
-export { default as CategoriesPage } from './CategoriesPage';
-export { default as CouponsPage } from './CouponsPage';
-export { default as OrdersPage } from './OrdersPage';
-export { default as ProductsPage } from './ProductsPage';
-export { default as ReportsPage } from './ReportsPage';
-export { default as SettingsPage } from './SettingsPage';
